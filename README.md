e7a0797b491d14d933cbc07ed764c76685ea2be4
Miguel-Deza <dmikec101@gmail.com>
2024-09-20 14:56:25 -0500

    finish

---
+++ README.md
@@ -0,0 +1,24 @@
+<video width="600" controls>
+  <source src="./chrome_lH0BIEDg45.mp4" type="video/mp4">
+  Tu navegador no soporta la etiqueta de video.
+</video>
+
+Hola a todos! 😄👋
+
+¡Estoy emocionado de compartir con ustedes mi nuevo portafolio web! 🌐🎉 Aquí es donde podrán ver un poco más sobre mí y los proyectos en los que he trabajado como desarrollador web 💻. Mi objetivo principal con este portafolio es mostrar mi experiencia, mis habilidades en desarrollo frontend 🚀, y cómo transformo ideas en experiencias de usuario increíbles ✨.
+
+### ¿Qué incluye mi portafolio? 💼
+- 🌟 **Proyectos destacados**: Algunos de los trabajos más relevantes en los que he participado.
+- 💡 **Habilidades técnicas**: Tecnologías y lenguajes que manejo en mi día a día como desarrollador.
+- 🧑‍💻 **Sobre mí**: Un poco de mi historia como desarrollador y mi enfoque en la creación de soluciones web.
+- 📫 **Contacto**: Formas de conectarte conmigo si te gustaría colaborar o simplemente charlar sobre tecnología.
+
+### ¿Por qué este portafolio? 🤔
+Este proyecto es mi forma de consolidar todas mis habilidades en un solo lugar 🌍, además de ser un espacio para que cualquiera pueda ver cómo trabajo y en qué estoy enfocado. Mi portafolio está desarrollado con **Next.js** y diseñado para ser rápido, interactivo y visualmente atractivo 🎨🚀.
+
+### ¡Visítalo y dame tu opinión! ✨
+
+Estoy constantemente buscando mejorar y agregar nuevas cosas, así que cualquier feedback será muy apreciado 🙌. ¡Espero que lo disfruten tanto como yo disfruté creándolo! 
+
+Gracias por pasarte por aquí y echarle un vistazo 😁🎉!
+
---
+++ components/Hero.tsx
@@ -0,0 +1,87 @@
+import { FaLocationArrow } from "react-icons/fa6";
+
+import MagicButton from "./MagicButton";
+import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      {/**
+       *  UI: Spotlights
+       *  Link: https://ui.aceternity.com/components/spotlight
+       */}
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+
+      {/**
+       *  UI: grid
+       *  change bg color to bg-black-100 and reduce grid color from
+       *  0.2 to 0.03
+       */}
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        {/* Radial gradient for the container to give a faded look */}
+        <div
+          // change the bg to bg-black-100, so it matches the bg color and will blend in
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
+      </div>
+
+      {/* Main container with flex layout */}
+      <div className="flex justify-between relative my-20 z-10 max-w-6xl mx-auto">
+        {/* Left column: Text content */}
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            From Perú 
+          </p>
+
+          {/**
+           *  Link: https://ui.aceternity.com/components/text-generate-effect
+           *
+           *  change md:text-6xl, add more responsive code
+           */}
+          <TextGenerateEffect
+            words="Transforming Concepts into Seamless User Experiences"
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+          />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Miguel Deza, a Web Developer based in Perú.
+          </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
+        </div>
+
+        {/* Right column: Profile picture */}
+        <div className="flex-1 flex items-center justify-center">
+          <img
+            src="/miguel_deza.png" // Reemplaza con la ruta de tu foto de perfil
+            alt="Profile Picture"
+            className="w-80 h-80 rounded-full object-cover shadow-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
